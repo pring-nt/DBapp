039bda0d4f984cda1d8d9be1cfd3b5d71579d05b
nathan <natetrndd@gmail.com>
2025-11-05 23:02:19 +0800

    chore: add some comments

--- src/main/java/com/gymdb/model/Attendance.java
+++ src/main/java/com/gymdb/model/Attendance.java
@@ -1,7 +1,10 @@
 package com.gymdb.model;
 import java.time.LocalDateTime;
 
-public record Attendance(int attendanceID, LocalDateTime datetime, int memberID, int classID) {
-
-}
+public record Attendance(
+        int attendanceID,
+        LocalDateTime datetime,
+        int memberID,
+        int classID
+) {}
 
